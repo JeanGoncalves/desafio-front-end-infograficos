bd0936cd60e786c9360a11263f8b851971db7267
Jean Gonçalves <jeanfpgoncalves@live.com>
2017-06-22 17:10:00 -0300

    Implement Map

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { AgmCoreModule } from '@agm/core';
 
 import { SlideComponent } from './slide/slide.component';
 import { NewsComponent } from './news/news.component';
@@ -10,7 +11,10 @@ import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB4FQ9Lmt4IwhYpeFoK_YDKvxsDuX_21qk'
+    })
   ],
   declarations: [
     HomeComponent, 
--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  public lat: number = 51.521356;
+  public lng: number = -0.157307;
+  public zoom: number = 16;
+  public iconMarker: string = '/assets/images/iconMarker.png';
+
   constructor() { }
 
   ngOnInit() {
